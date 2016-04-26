34a42582d115d5da7eee022163329896fd058a63
svexican <svexican@gmail.com>
2016-04-25 21:42:33 -0500

    icons revisited (coloured white)

--- javascripts/country.js
+++ javascripts/country.js
@@ -221,6 +221,27 @@ function load_everything(err, all_countries, world_topo, transmission_lines, pla
     //Initialize tooltip
     $('[data-toggle="tooltip"]').tooltip();
   });
+
+  jQuery('.tier-icon').each(function() {
+    var $img = jQuery(this);
+    var imgID = $img.attr('id');
+    var imgClass = $img.attr('class');
+    var imgURL = $img.attr('src');
+
+    jQuery.get(imgURL, function(data) {
+      // Get the SVG tag, ignore the rest
+      var $svg = jQuery(data).find('svg');
+
+      // Add replaced image's ID to the new SVG
+      if(typeof imgID !== 'undefined') { $svg = $svg.attr('id', imgID); }
+
+      // Add replaced image's classes to the new SVG
+      if(typeof imgClass !== 'undefined') { $svg = $svg.attr('class', imgClass+' replaced-svg'); }
+
+      // Replace image with new SVG
+      $img.replaceWith($svg);
+    }, 'xml');
+  });
 }
 
 queue(4)
